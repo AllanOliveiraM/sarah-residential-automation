c3eeb133621500a114e002e3d37f3fed7687e661
AllanOliveiraM <nallamo2016@gmail.com>
2020-03-28 05:25:17 -0300

    Bug fixed in to run install command

--- install_workstation_linux.sh
+++ install_workstation_linux.sh
@@ -1,1 +1,1 @@
-python3 ./modules/dependency-manager/module_install_workstation_linux.py && sh ./venv/bin/activate && ./venv/bin/python -m pip install --upgrade pip && ./venv/bin/python -m pip install -r requirements.txt && echo " " && echo "Done!"
+python3 ./modules/dependency-manager/module_install_workstation_linux.py && echo "Automatically continuing..." && sh ./venv/bin/activate && ./venv/bin/python -m pip install --upgrade pip && ./venv/bin/python -m pip install -r requirements.txt && echo " " && echo "Done!"
